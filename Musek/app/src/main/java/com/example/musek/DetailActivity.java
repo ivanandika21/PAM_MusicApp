dc7a023f003f82721de8f5ff5005d06f15439bd8
ivanandika21 <ivanandikasurya21@gmail.com>
2022-03-18 19:21:42 +0700

    Musek - done playing, next, prev, seekbar

--- Musek/app/src/main/java/com/example/musek/DetailActivity.java
+++ Musek/app/src/main/java/com/example/musek/DetailActivity.java
@@ -2,11 +2,14 @@ package com.example.musek;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
 import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 
@@ -17,6 +20,8 @@ public class DetailActivity extends AppCompatActivity {
     ImageView var_detailImage, var_fr, var_prev, var_pauseplay, var_next, var_ff;
     ArrayList<DataModel> laguList;
     DataModel currentLagu;
+    MediaPlayer mediaPlayer = LaguPlayer.getInstance();
+    int rot = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +47,44 @@ public class DetailActivity extends AppCompatActivity {
         laguList = (ArrayList<DataModel>) getIntent().getSerializableExtra("list");
 
         setResourcesWithMusic();
+
+        DetailActivity.this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                if (mediaPlayer != null) {
+                    var_seekbar.setProgress(mediaPlayer.getCurrentPosition());
+                    var_txtStart.setText(convertToMS(mediaPlayer.getCurrentPosition()+""));
+
+                    if (mediaPlayer.isPlaying()) {
+                        var_pauseplay.setImageResource(R.drawable.ic_pause);
+                        var_detailImage.setRotation(rot++);
+                    } else {
+                        var_pauseplay.setImageResource(R.drawable.ic_play);
+                        var_detailImage.setRotation(0);
+                    }
+                }
+                new Handler().postDelayed(this, 100);
+            }
+        });
+
+        var_seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                if (mediaPlayer != null && b) {
+                    mediaPlayer.seekTo(i);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 
     void setResourcesWithMusic() {
@@ -59,15 +102,45 @@ public class DetailActivity extends AppCompatActivity {
 
     private void playLagu() {
 
+        mediaPlayer.reset();
+        try {
+            mediaPlayer.setDataSource(currentLagu.getPath());
+            mediaPlayer.prepare();
+            mediaPlayer.start();
+            var_seekbar.setProgress(0);
+            var_seekbar.setMax(mediaPlayer.getDuration());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
     private void nextLagu() {
 
+        if (LaguPlayer.currentIndex == laguList.size()-1) {
+            return;
+        }
+        LaguPlayer.currentIndex +=1;
+        mediaPlayer.reset();
+        setResourcesWithMusic();
     }
+
     private void prevLagu() {
 
+        if (LaguPlayer.currentIndex == 0) {
+            return;
+        }
+        LaguPlayer.currentIndex -=1;
+        mediaPlayer.reset();
+        setResourcesWithMusic();
     }
+
     private void pausePlayLagu() {
 
+        if (mediaPlayer.isPlaying()){
+            mediaPlayer.pause();
+        } else {
+            mediaPlayer.start();
+        }
     }
 
     public static String convertToMS(String duration) {
